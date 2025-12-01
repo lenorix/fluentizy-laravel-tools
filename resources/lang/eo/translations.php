f962fc224d9c931275bd01a307f84c8a42e7e663
Jesus Hernandez <jesushdez@protonmail.com>
2025-12-01 22:46:16 +0100

    add translations for Esperanto and Interlingua

---
+++ resources/lang/eo/translations.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'locale-error' => 'Neniu locale estis specifita kaj neniuj lingvaj dosieroj estis trovataj en :path',
+    'ready' => ':emoji Preta :path',
+];
---
+++ resources/lang/ia/translations.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'locale-error' => 'Nulle locale esseva specificate e nulle files de lingua esseva trovate in :path',
+    'ready' => ':emoji Preste :path',
+];
