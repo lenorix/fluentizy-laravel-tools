da7d843584aa340f63a35b2fd5f199cda017ce98
Jesus Hernandez <jesushdez@protonmail.com>
2025-12-03 02:08:31 +0100

    fix typo in translation message for clarity

--- resources/lang/en/translations.php
+++ resources/lang/en/translations.php
@@ -2,7 +2,7 @@
 
 return [
     'complete' => 'Now you can translate your application!
-Consider use https://fluentizy.lenorix.com 😃',
+Consider using https://fluentizy.lenorix.com 😃',
     'locale-error' => 'No locale specified and no lang files found in :path',
     'ready' => ':emoji  Ready :path',
 ];
